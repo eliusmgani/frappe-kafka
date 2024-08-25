21eb658433b55ce3af1cb088acd211cda4a33428
eliusmgani <eliasmgani@gmail.com>
2024-08-26 00:26:02 +0300

    feat: kafka settings to store cluster informations like api key and secret key

---
+++ frappe_kafka/frappe_kafka/doctype/kafka_settings/kafka_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, eliusmgani and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Kafka Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
