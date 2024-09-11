403ff90f809a9d197f58082cfaa3b222a7e7933e
eliusmgani <eliasmgani@gmail.com>
2024-09-11 15:33:37 +0300

    feat: allow selecting fields for the source data doctype

--- frappe_kafka/frappe_kafka/doctype/kafka_connector/kafka_connector.js
+++ frappe_kafka/frappe_kafka/doctype/kafka_connector/kafka_connector.js
@@ -2,7 +2,84 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Kafka Connector', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+	select_fields: function(frm) {
+		frm.events.field_selector(frm);
+	},
+	field_selector: (frm) => {
+		let document_fields = [];
+		if (frm.doc.selected_fields)
+			document_fields = (JSON.parse(frm.doc.selected_fields)).map(f => f.fieldname);
+
+		frm.call({
+			method: 'get_source_fields',
+			doc: frm.doc,
+			args: {
+				fields: document_fields
+			},
+			freeze: true,
+			freeze_message: __('Fetching fields...'),
+			callback: (r) => {
+				if (r.message) {
+					let doc_fields = r.message;
+					frm.events.show_field_selector_dialog(frm, doc_fields);
+				}
+			}
+		});
+	},
+	show_field_selector_dialog: (frm, doc_fields) => {
+		let d = new frappe.ui.Dialog({
+			title: __('{0} Fields', [__(frm.doc.data_source)]),
+			fields: [
+				{
+					label: __('Select Fields'),
+					fieldtype: 'MultiCheck',
+					fieldname: 'fields',
+					options: doc_fields,
+					columns: 4
+				}
+			]
+		});
+
+		d.$body.prepend(`
+			<div class="columns-search">
+				<input type="text" placeholder="${__('Search')}" data-element="search" class="form-control input-xs">
+			</div>`
+		);
+
+		frappe.utils.setup_search(d.$body, '.unit-checkbox', '.label-area');
+
+		d.set_primary_action(__('Save'), () => {
+			let values = d.get_values().fields;
+
+			let selected_fields = [];
+
+			frappe.model.with_doctype(frm.doc.data_source, function() {
+				for (let idx in values) {
+					let value = values[idx];
+
+					let field = frappe.get_meta(frm.doc.data_source).fields.filter((df) => df.fieldname == value)[0];
+					console.log(field);
+					if (field) {
+						selected_fields.push({
+							label: field.label,
+							fieldname: field.fieldname,
+							fieldtype: field.fieldtype
+						});
+					}
+				}
+
+				d.refresh();
+				frappe.model.set_value(frm.doc.doctype, frm.doc.name, 'selected_fields', JSON.stringify(selected_fields));
+			});
+
+			d.hide();
+		});
+
+		d.$wrapper.find('.modal-content').css('width', '900px')
+
+		d.show();
+	}
 });
--- frappe_kafka/frappe_kafka/doctype/kafka_connector/kafka_connector.py
+++ frappe_kafka/frappe_kafka/doctype/kafka_connector/kafka_connector.py
@@ -1,8 +1,32 @@
 # Copyright (c) 2024, eliusmgani and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model import no_value_fields, table_fields
 
 class KafkaConnector(Document):
-	pass
+	@frappe.whitelist()
+	def get_source_fields(self, fields):
+		"""Get fields from the source document"""
+
+		multicheck_fields = []
+		doc_fields = frappe.get_meta(self.data_source).fields
+
+		for field in doc_fields:
+			if (
+				(
+					field.fieldtype not in no_value_fields
+					or field.fieldtype in table_fields
+				)
+				and not field.hidden
+			):
+				multicheck_fields.append(
+					{
+						"label": field.label,
+						"value": field.fieldname,
+						"checked": 1 if field.fieldname in fields else 0,
+					}
+				)
+		
+		return multicheck_fields
