bb1a95193c51691e766d1cee07d25f2b2949b08b
eliusmgani <eliasmgani@gmail.com>
2024-12-26 18:31:07 +0300

    feat: validate condition and doc events for kafka connector

--- frappe_kafka/frappe_kafka/doctype/kafka_connector/kafka_connector.py
+++ frappe_kafka/frappe_kafka/doctype/kafka_connector/kafka_connector.py
@@ -2,10 +2,35 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils.safe_exec import get_safe_globals
 from frappe.model import no_value_fields, table_fields
 
 class KafkaConnector(Document):
+	def validate(self):
+		self.validate_docevent()
+		self.validate_condition()
+	
+	def validate_docevent(self):
+		if self.data_source:
+			is_submittable = frappe.get_cached_value("DocType", self.data_source, "is_submittable")
+			if not is_submittable:
+				submittable_events = ["on_submit", "on_cancel", "on_update_after_submit"]
+
+				for row in self.events:
+					if row.event_name in submittable_events:
+						frappe.throw(_(f"DocType must be Submittable for the selected Doc Event: {row.event_name}"))
+
+
+	def validate_condition(self):
+		temp_doc = frappe.new_doc(self.data_source)
+		if self.condition:
+			try:
+				frappe.safe_eval(self.condition, eval_locals=get_context(temp_doc))
+			except Exception as e:
+				frappe.throw(_(f"Invalid Condition: {e}"))
+	
 	@frappe.whitelist()
 	def get_source_fields(self, fields):
 		"""Get fields from the source document"""
@@ -30,3 +55,7 @@ class KafkaConnector(Document):
 				)
 		
 		return multicheck_fields
+
+
+def get_context(doc):
+	return {"doc": doc, "utils": get_safe_globals().get("frappe").get("utils")}
